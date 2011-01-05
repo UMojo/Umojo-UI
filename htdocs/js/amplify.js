1da8ddc8b4b415b872670ce332bc4e789d9c33d7
dschreiber <d@d-man.org>
2011-01-05 10:05:40 -0800

    New call flow module

--- htdocs/js/amplify.js
+++ htdocs/js/amplify.js
@@ -717,7 +717,7 @@ amplify.module = function(module, config, construct, methods) {
 };
 
 amplify.module.load = function(module, callback) {
-	$LAB.script('/module/' + module + '/' + module + '.js')
+	$LAB.script('module/' + module + '/' + module + '.js')
 		.wait(function() {
 			callback.call( amplify.module(module) );
 		});
--- htdocs/js/winkstart.js
+++ htdocs/js/winkstart.js
@@ -12,7 +12,7 @@
 			this.templates = {};
 			$.each(this.config.templates, function(name, url) {
 				completed++;
-				$.get('/module/' + THIS.__module + '/' + url, function(template) {
+				$.get('module/' + THIS.__module + '/' + url, function(template) {
 					console.log('Loaded template ' + name + ' at ' + url);
 					completed--;
 					THIS.templates[name] = $(template);
@@ -33,7 +33,7 @@
 				if ( css === true ) {
 					THIS.__module + '.css';
 				}
-				css = '/module/' + THIS.__module + '/' + css;
+				css = 'module/' + THIS.__module + '/' + css;
 				//completed++;
 				console.log('Loading css: ' + css);
 				$('<link href="' + css + '" rel="stylesheet" type="text/css">').bind('load', function() {
@@ -68,147 +68,7 @@
 			setTimeout(function() { _c(); }, 10);
 		})();
 	};
-	
-	/*
-	winkstart.module = {
-		getResourcePath: function(module, resource) {
-			return '/module/' + module + '/' + resource;
-		},
 		
-		// 
-		define: function(module, definition) {
-			// Check if we need to execute the method to generate it
-			if ( $.isFunction(definition) ) {
-				definition = definition.call({});
-			}
-			
-			var m = modules[module] = definition;
-			if ( m.onDefine && $.isFunction(m.onDefine) ) {
-				// Make onDefine async?
-				m.onDefine.call(m);
-			}
-			
-			// Load required module dependencies
-			if ( m.requires ) {
-				var modulesToLoad = $.makeArray(m.requires);
-				(function queuedLoad() {
-					var next = modulesToLoad.shift();
-					winkstart.module.using(next, 
-						function() {
-							if ( toLoad.length > 0 ) {
-								queuedLoad();
-							} else {
-								continueDefine();
-							}
-						},
-						function() {
-							alert('An error occurred');
-						});
-				})();
-			} else {
-				
-			}
-			
-			function continueDefine() {
-				// Load the required assets
-				
-				
-				// Define any request types
-				if ( m.requests ) {
-					$.each(m.requests, function(k, v) {
-						var type = 'ajax', args = { method: 'POST' };
-						if ( $.isPlainObject(v) ) {
-							args = v;
-							if ( v.type ) {
-								type = v.type;
-							}
-						} else {
-							args.url = v;
-						}
-						amplify.request.define(k, type, args);
-					});
-				}
-				
-				
-				// Define exported interfaces from the module
-				if ( m.exports && $.isFunction(m.exports) ) {
-					var exported = m.exports.call(m);
-					if ( exported ) {
-						$.extend( winkstart[module] = winkstart[module] || {}, exported);
-					}
-				}
-			}
-		},
-
-		// This will load the required module and then execute the callback
-		using: function(module, callback, errorCallback) {
-			// Make sure the module is loading code
-			
-			var m = $.makeArray(module);
-			for ( var i = 0; i < m.length; i++ ) {
-				module = m[i];
-				if ( !modules[module] ) {
-					// We need to go out and load the module
-					if ( loading[module] ) {
-						loading[module].callbacks.push(callback);
-						
-						if ( $.isFunction(errorCallback) ) {
-							loading[module].errorCallbacks.push(errorCallback);
-						}
-						
-					} else {
-						loading[module] = {
-							loading: true,
-							callbacks: [callback],
-							errorCallbacks: []
-						};
-						
-						if ( $.isFunction(errorCallback) ) {
-							loading[module].errorCallbacks.push(errorCallback);
-						}
-						
-						// Determine the module URL
-						var url = this.getResourcePath(module, module + '.js');
-						$.getScript(url, function() {
-							// The module has supposably been loaded (there should now be a definition in modules
-							if ( !modules[module] ) {
-								var cbs = loading[module].errorCallbacks;
-							} else {
-								// The module is now loaded, go through and execute each callback that is queued
-								var cbs = loading[module].callbacks;
-								try {
-									loading[module] = null;
-									delete loading[module];
-								} catch (e) {}
-							}
-							
-							for ( var f in cbs ) {
-								cbs[f].call(window);
-							}
-						});
-					}
-				} else {
-					// TODO: Error loading module
-				}
-			}
-		},
-		
-		load: function(module, callback) {
-		
-		},
-
-		// Method for 
-		error: function(callback) {
-			alert('An error has occurred');
-		}
-	};
-	
-	winkstart.plugins = {
-		register: function() {
-		}
-	};
-	*/
-	
 	// Bootstrap the app: Start by loading the core module
 	winkstart.module.load('core', function() {
 		// Currently core doesn't do anything, it will
@@ -228,9 +88,19 @@
 					winkstart.module.load('dashboard', function() {
 						this.init(function() {
 							// Activate the dashboard by default (target selector is a hack)
-							winkstart.publish('dashboard.activate', { target: $('#ws-content') });
+							//winkstart.publish('dashboard.activate', { target: $('#ws-content') });
 						});
 					});
+                                        winkstart.module.load('provisioner', function() {
+                                                this.init(function(){
+                                                   //winkstart.publish('provisioner.activate', { target: $('#ws-content') });
+                                                });
+                                        });
+                                        winkstart.module.load('callflow', function() {
+                                                this.init(function(){
+                                                   winkstart.publish('callflow.activate', { target: $('#ws-content') });
+                                                });
+                                        });
 				});
 			});
 		});
