ded4f0811ba24be02cd356fee398835317b973a9
Francis Genet & Peter Defebvre <francis.peter@2600hz.com>
2011-07-19 16:25:47 -0700

    Get data CDR and put them in the table

--- htdocs/whapps/dashboard/ctt/ctt.js
+++ htdocs/whapps/dashboard/ctt/ctt.js
@@ -6,7 +6,7 @@ winkstart.module('dashboard', 'ctt',
 
         /* What HTML templates will we be using? */
         templates: {
-            registration: 'tmpl/ctt.html'//,
+            ctt: 'tmpl/ctt.html'//,
         //detailRegistration: 'tmpl/detailRegistration.html'
         },
 
@@ -24,13 +24,13 @@ winkstart.module('dashboard', 'ctt',
 
         /* What API URLs are we going to be calling? Variables are in { }s */
         resources: {
-            "registration.list": {
-                url: CROSSBAR_REST_API_ENDPOINT + '/accounts/{account_id}/registrations',
+            "cdr.list": {
+                url: CROSSBAR_REST_API_ENDPOINT + '/accounts/{account_id}/cdr',
                 contentType: 'application/json',
                 verb: 'GET'
             },
-            "registration.read": {
-                url: CROSSBAR_REST_API_ENDPOINT + '/accounts/{account_id}/registrations/{registration_id}',
+            "cdr.read": {
+                url: CROSSBAR_REST_API_ENDPOINT + '/accounts/{account_id}/cdr/{cdr_id}',
                 contentType: 'application/json',
                 verb: 'GET'
             }
@@ -61,136 +61,128 @@ winkstart.module('dashboard', 'ctt',
             
             winkstart.loadFormHelper('forms');
 
-            this.templates.registration.tmpl({}).appendTo( $('#ws-content') );
+            this.templates.ctt.tmpl({}).appendTo( $('#ws-content') );
             
             var num_rows = 0;
             
             //winkstart.getJSON('registration.list', {crossbar: true, account_id: MASTER_ACCOUNT_ID}, function(reply) {
-            winkstart.getJSON('registration.list', {
+            winkstart.getJSON('cdr.list', {
                 crossbar: true, 
                 account_id: '04152ed2b428922e99ac66f3a71b0215'
             }, function(reply) {
                 THIS.setup_table();
                 $.each(reply.data, function() {
-                    var registration_id = this.id;
+                    var cdr_id = this.id;
                     
                     num_rows = num_rows+1;
 
                     //winkstart.getJSON('registration.read',{crossbar: true, account_id: MASTER_ACCOUNT_ID, registration_id: registration_id}, function(reply) {
-                    winkstart.getJSON('registration.read',{
+                    winkstart.getJSON('cdr.read',{
                         crossbar: true, 
                         account_id: '04152ed2b428922e99ac66f3a71b0215', 
-                        registration_id: registration_id
+                        cdr_id: cdr_id
                     }, function(reply) {
                         if(reply.data == undefined) {
                             return false;
                         }
 
                         //Dumb hack before crossbar reply is normalized (with lower case and _)
-                        if(reply.data['App-Name'] != undefined) {
-                            reply.data.app_name = reply.data['App-Name'];
+                        if(reply.data['app_name'] != undefined) {
+                            reply.data.app_name = reply.data['app_name'];
                         }
-                        if(reply.data['App-Version'] != undefined) {
-                            reply.data.app_version = reply.data['App-Version'];
+                        if(reply.data['app_version'] != undefined) {
+                            reply.data.app_version = reply.data['app_version'];
                         }
-                        if(reply.data['Call-ID'] != undefined) {
-                            reply.data.call_id = reply.data['Call-ID'];
+                        if(reply.data['billing_seconds'] != undefined) {
+                            reply.data.billing_seconds = reply.data['billing_seconds'];
                         }
-                        if(reply.data['Contact'] != undefined) {
-                            reply.data.contact = reply.data['Contact'];
+                        if(reply.data['call_direction'] != undefined) {
+                            reply.data.call_direction = reply.data['call_direction'];
                         }
-                        if(reply.data['Event-Category'] != undefined) {
-                            reply.data.event_category = reply.data['Event-Category'];
+                        if(reply.data['call_id'] != undefined) {
+                            reply.data.call_id = reply.data['call_id'];
                         }
-                        if(reply.data['Event-Name'] != undefined) {
-                            reply.data.event_name = reply.data['Event-Name'];
+                        if(reply.data['callee_id_name'] != undefined) {
+                            reply.data.callee_id_name = reply.data['callee_id_name'];
                         }
-                        if(reply.data['Event-Timestamp'] != undefined) {
-                            reply.data.event_timestamp = reply.data['Event-Timestamp'];
+                        if(reply.data['callee_id_number'] != undefined) {
+                            reply.data.callee_id_number = reply.data['callee_id_number'];
                         }
-                        if(reply.data['Expires'] != undefined) {
-                            reply.data.expires = reply.data['Expires'];
+                        if(reply.data['caller_id_name'] != undefined) {
+                            reply.data.caller_id_name = reply.data['caller_id_name'];
                         }
-                        if(reply.data['FreeSWITCH-Hostname'] != undefined) {
-                            reply.data.freeswitch_hostname = reply.data['FreeSWITCH-Hostname'];
+                        if(reply.data['caller_id_number'] != undefined) {
+                            reply.data.caller_id_number = reply.data['caller_id_number'];
                         }
-                        if(reply.data['From-Host'] != undefined) {
-                            reply.data.from_host = reply.data['From-Host'];
+                        if(reply.data['digits_dialed'] != undefined) {
+                            reply.data.digits_dialed = reply.data['digits_dialed'];
                         }
-                        if(reply.data['From-User'] != undefined) {
-                            reply.data.from_user = reply.data['From-User'];
+                        if(reply.data['duration_seconds'] != undefined) {
+                            reply.data.duration_seconds = reply.data['digits_dialed'];
                         }
-                        if(reply.data['Network-IP'] != undefined) {
-                            reply.data.network_ip = reply.data['Network-IP'];
+                        if(reply.data['event_category'] != undefined) {
+                            reply.data.event_category = reply.data['event_category'];
                         }
-                        if(reply.data['Network-Port'] != undefined) {
-                            reply.data.network_port = reply.data['Network-Port'];
+                        if(reply.data['event_name'] != undefined) {
+                            reply.data.event_name = reply.data['event_name'];
                         }
-                        if(reply.data['Presence-Hosts'] != undefined) {
-                            reply.data.presence_hosts = reply.data['Presence-Hosts'];
+                        if(reply.data['from_uri'] != undefined) {
+                            reply.data.from_uri = reply.data['from_uri'];
                         }
-                        if(reply.data['Profile-Name'] != undefined) {
-                            reply.data.profile_name = reply.data['Profile-Name'];
+                        if(reply.data['handling_server_name'] != undefined) {
+                            reply.data.handling_server_name = reply.data['handling_server_name'];
                         }
-                        if(reply.data['RPid'] != undefined) {
-                            reply.data.rpid = reply.data['RPid'];
+                        if(reply.data['hangup_cause'] != undefined) {
+                            reply.data.hangup_cause = reply.data['hangup_cause'];
                         }
-                        if(reply.data['Realm'] != undefined) {
-                            reply.data.realm = reply.data['Realm'];
+                        if(reply.data['id'] != undefined) {
+                            reply.data.id = reply.data['id'];
                         }
-                        if(reply.data['Server-ID'] != undefined) {
-                            reply.data.server_id = reply.data['Server-ID'];
+                        if(reply.data['local_sdp'] != undefined) {
+                            reply.data.local_sdp = reply.data['local_sdp'];
                         }
-                        if(reply.data['Status'] != undefined) {
-                            reply.data.status = reply.data['Status'];
+                        if(reply.data['remote_sdp'] != undefined) {
+                            reply.data.remote_sdp = reply.data['remote_sdp'];
                         }
-                        if(reply.data['To-Host'] != undefined) {
-                            reply.data.to_host = reply.data['To-Host'];
+                        if(reply.data['ringing_seconds'] != undefined) {
+                            reply.data.ringing_seconds = reply.data['ringing_seconds'];
                         }
-                        if(reply.data['To-User'] != undefined) {
-                            reply.data.to_user = reply.data['To-User'];
+                        if(reply.data['server_id'] != undefined) {
+                            reply.data.server_id = reply.data['server_id'];
                         }
-                        if(reply.data['User-Agent'] != undefined) {
-                            reply.data.user_agent = reply.data['User-Agent'];
+                        if(reply.data['timestamp'] != undefined) {
+                            reply.data.timestamp = reply.data['timestamp'];
                         }
-                        if(reply.data['Username'] != undefined) {
-                            reply.data.username = reply.data['Username'];
-                        }                       
-
-                        var friendlyDate = new Date((reply.data.event_timestamp - 62167219200)*1000);
-                        var humanDate = friendlyDate.toLocaleDateString();
-                        var humanTime = friendlyDate.toLocaleTimeString(); 
+                        if(reply.data['to_uri'] != undefined) {
+                            reply.data.to_uri = reply.data['to_uri'];
+                        }
+                        if(reply.data['user_agent'] != undefined) {
+                            reply.data.user_agent = reply.data['user_agent'];
+                        }
+                        
                                 
                         console.log(reply.data);
      
-                        var stringToDisplay = 'Details of Registration\\n';
-                        stringToDisplay += '\\nid: ' + reply.data.id; 
-                        stringToDisplay += '\\nApp-Name: ' + reply.data.app_name;
-                        stringToDisplay += '\\nApp-Version: ' + reply.data.app_version;
-                        stringToDisplay += '\\nCall-ID: ' + reply.data.call_id;
-                        //stringToDisplay += '\\nContact: ' + reply.data.contact;
-                        stringToDisplay += '\\nEvent-Category: ' + reply.data.event_category;
-                        stringToDisplay += '\\nEvent-Name: ' + reply.data.event_name;
-                        stringToDisplay += '\\nExpires: ' + reply.data.expires;
-                        stringToDisplay += '\\nFreeSWITCH-Hostname: ' + reply.data.freeswitch_hostname;
-                        stringToDisplay += '\\nFrom-Host: ' + reply.data.from_host;
-                        stringToDisplay += '\\nFrom-User: ' + reply.data.from_user;
-                        stringToDisplay += '\\nNetwork-IP: ' + reply.data.network_ip;
-                        stringToDisplay += '\\nNetwork-Port: ' + reply.data.network_port;
-                        stringToDisplay += '\\nPresence-Hosts: ' + reply.data.presence_hosts;
-                        stringToDisplay += '\\nProfile-Name: ' + reply.data.profile_name;
-                        stringToDisplay += '\\nRPid: ' + reply.data.rpid;
-                        stringToDisplay += '\\nRealm: ' + reply.data.realm;
-                        stringToDisplay += '\\nServer-ID: ' + reply.data.server_id;
-                        stringToDisplay += '\\nStatus: ' + reply.data.status;
-                        stringToDisplay += '\\nTo-Host: ' + reply.data.to_host;
-                        stringToDisplay += '\\nTo-User: ' + reply.data.to_user;
-                        stringToDisplay += '\\nUser-Agent: ' + reply.data.user_agent;
-                        stringToDisplay += '\\nUsername: ' + reply.data.username;
-                        stringToDisplay += '\\nDate: ' + humanDate;
-                        stringToDisplay += '\\nTime: ' + humanTime;
-
-                        winkstart.table.registration.fnAddData([reply.data.username, reply.data.network_ip, reply.data.network_port, humanDate, humanTime, stringToDisplay]);
+                        var stringToDisplay = '';
+                        
+                        function noData(data){
+                            if(data == null){
+                                return 'No data';
+                            }else{
+                                return data;
+                            }
+                        }
+                        
+                        winkstart.table.ctt.fnAddData([
+                            noData(reply.data.id),
+                            noData(reply.data.callee_id_number),
+                            noData(reply.data.caller_id_number),
+                            noData(reply.data.duration_seconds),
+                            noData(reply.data.hangup_cause),
+                            'Debug',
+                            'Details',
+                            'blabla',
+                        ]);
                     });
                 });
                 
@@ -209,40 +201,52 @@ winkstart.module('dashboard', 'ctt',
             var THIS = this;
             var columns = [
             {
-                'sTitle': 'Username'
+                'sTitle': 'Call id'
             },
 
             {
-                'sTitle': 'IP'
+                'sTitle': 'Called to'
             },
 
             {
-                'sTitle': 'Port'
+                'sTitle': 'Called from'
             },
 
             {
-                'sTitle': 'Date'
+                'sTitle': 'Call duration'
             },
 
             {
-                'sTitle': 'Time'
+                'sTitle': 'Hangup cause'
+            },
+            
+            {
+                'sTitle': 'Debug'
+            },
+            
+            {
+                'sTitle': 'Details'
+            },
+            
+            {
+                'sTitle': 'Show other leg'
             },
 
-            {
-                'sTitle': 'Details',
-                'fnRender': function(obj) {
-                    console.log(obj);
-                    var reg_details = obj.aData[obj.iDataColumn];
-                    return '<a href="#" onClick="alert(\''+reg_details+'\');">Details</a>';
-                }
-            }
+            //            {
+            //                'sTitle': 'Details',
+            //                'fnRender': function(obj) {
+            //                    console.log(obj);
+            //                    var reg_details = obj.aData[obj.iDataColumn];
+            //                    return '<a href="#" onClick="alert(\''+reg_details+'\');">Details</a>';
+            //                }
+            //            }
             ];
 
-            winkstart.table.create('registration', $('#registration-grid'), columns);
-            $('#registration-grid_filter input[type=text]').first().focus();
+            winkstart.table.create('ctt', $('#ctt-grid'), columns);
+            $('#ctt-grid_filter input[type=text]').first().focus();
 			
             $('.cancel-search').click(function(){
-                $('#registration-grid_filter input[type=text]').val('');
+                $('#ctt-grid_filter input[type=text]').val('');
             });
             
             
