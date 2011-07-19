d9d9138420603ed4f5c1205c355378947c6b4562
Francis Genet & Peter Defebvre <francis.peter@2600hz.com>
2011-07-19 15:04:39 -0700

    Add skeleton tracetool in navbar dashboard

--- htdocs/whapps/dashboard/dashboard.js
+++ htdocs/whapps/dashboard/dashboard.js
@@ -1,34 +1,46 @@
-// This is the dashboard module, for monitoring services across the system
 winkstart.module('dashboard', 'dashboard', {
-        subscribe: {
-            'dashboard.activate' : 'activate'
-        }
-    },
-    function() {
-        // Loaded - add to nav bar
-        winkstart.publish('appnav.add', { 'name' : 'dashboard' });
-    },
-    {
-        initialized :   false,
-        modules :       ['monitor' ],
-            
-        activate: function() {
-            /*if (this.initialized) {
-                return;
-            }*/
-
-            winkstart.publish('subnav.clear');
-
-            // We only initialize once
-            this.initialized = true;
-
-            $.each(this.modules, function(k, v) {
-                winkstart.module.loadPlugin('dashboard', v, function() {
-                    this.init(function() {
-                        winkstart.log('Dashboard: Initialized ' + v);
-                    });
-                });
-            });
-        }
+    subscribe: {
+        'dashboard.activate' : 'activate'
     }
+},
+function() {
+    var THIS = this;
+
+    // Loaded - add to nav bar
+    winkstart.publish('appnav.add', {
+        'name' : 'dashboard'
+    });
+
+    // Load the modules
+    $.each(THIS.modules, function(k, v) {
+        winkstart.module.loadPlugin('dashboard', v, function() {
+            this.init(function() {
+                winkstart.log('Dashboard: Initialized ' + v);
+            });
+        });
+    });
+},
+{
+    initialized :   false,
+    modules :       ['ctt', 'monitor'],
+        
+    activate: function() {
+        var THIS = this;
+            
+        if (!THIS.initialized) {
+
+            // Display the navbar
+            $('#ws-content').empty();
+
+            // Link the main buttons
+            $('.options #monitor').click(function() {
+                winkstart.publish('monitor.activate');
+            });
+
+            $('.options #ctt').click(function() {
+                winkstart.publish('ctt.activate');
+            });
+        } 
+    }
+}
 );
