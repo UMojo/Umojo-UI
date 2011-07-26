80bccf9bd40c6415089a456723d0aad941a5bf27
dschreiber <d@d-man.org>
2011-07-26 11:22:28 -0700

    Auth

--- htdocs/whapps/core/auth/auth.js
+++ htdocs/whapps/core/auth/auth.js
@@ -1,82 +1,80 @@
 winkstart.module('core', 'auth', {
-		session: {
-			authenticated: false,
-			username: '',
-			token: '',
-			expires: -1
-		},
+    session: {
+        authenticated: false,
+        username: '',
+        token: '',
+        expires: -1
+    },
 		
-		requests: {
-			'auth.establish' : 'api/v2/authentication/establish',
-			'auth.destroy'   : 'api/v2/authentication/destroy'
-		},
+    requests: {
+        'auth.establish' : 'api/v2/authentication/establish',
+        'auth.destroy'   : 'api/v2/authentication/destroy'
+    },
 		
-		events: {
-			'a.btn-authenticate' : 'authenticate'
-		},
+    events: {
+        'a.btn-authenticate' : 'authenticate'
+    },
 		
-		templates: {
-			login: 'login.html',
-                        thankyou: 'tmpl/thankyou.html',
-                        edit_billing: 'tmpl/edit_billing.html',
-                        recover_password: 'tmpl/recover_password.html',
-                        login: 'tmpl/login.html'
-		},
+    templates: {
+        login: 'login.html',
+        thankyou: 'tmpl/thankyou.html',
+        edit_billing: 'tmpl/edit_billing.html',
+        recover_password: 'tmpl/recover_password.html',
+        login: 'tmpl/login.html'
+    },
 		
-		subscribe: {
-			'auth.activate' : 'activate'
-		},
+    subscribe: {
+        'auth.activate' : 'activate'
+    },
 
-                resources: {
-                    /* User Management */
-                    "sipservice.addUser": {
-                        url: 'https://store.2600hz.com/v1/addUser',
-                        contentType: 'application/json',
-                        verb: 'POST'
-                    },
+    resources: {
+        /* User Management */
+        "sipservice.addUser": {
+            url: 'https://store.2600hz.com/v1/addUser',
+            contentType: 'application/json',
+            verb: 'POST'
+        },
 
-                    "sipservice.editUser": {
-                        url: 'https://store.2600hz.com/v1/editUser',
-                        contentType: 'application/json',
-                        verb: 'POST'
-                    },
+        "sipservice.editUser": {
+            url: 'https://store.2600hz.com/v1/editUser',
+            contentType: 'application/json',
+            verb: 'POST'
+        },
 
-                    "sipservice.getUser": {
-                        url: 'https://store.2600hz.com/v1/getUser',
-                        contentType: 'application/json',
-                        verb: 'POST'
-                    },
+        "sipservice.getUser": {
+            url: 'https://store.2600hz.com/v1/getUser',
+            contentType: 'application/json',
+            verb: 'POST'
+        },
 
-                    "sipservice.checkIfExists": {
-                        url: 'https://store.2600hz.com/v1/checkIfExists',
-                        contentType: 'application/json',
-                        verb: 'GET'
-                    },
+        "sipservice.checkIfExists": {
+            url: 'https://store.2600hz.com/v1/checkIfExists',
+            contentType: 'application/json',
+            verb: 'GET'
+        },
 
-                    "sipservice.getUserAuth": {
-                        url: 'https://store.2600hz.com/v1/getUserAuth',
-                        contentType: 'application/json',
-                        verb: 'POST'
-                    }
-                },
+        "sipservice.getUserAuth": {
+            url: 'https://store.2600hz.com/v1/getUserAuth',
+            contentType: 'application/json',
+            verb: 'POST'
+        }
+    },
 		
-		// Called when modules are to initialize
-		init: function() {
-			// Check if we already have a session stored in a cookie
+    // Called when modules are to initialize
+    init: function() {
+        // Check if we already have a session stored in a cookie
 			
-			var auth = $.cookie('winkstart');
-			if ( auth ) {
-				this.session = auth;
-			}
-		},
+        var auth = $.cookie('winkstart');
+        if ( auth ) {
+            this.session = auth;
+        }
+    },
 		
-		/*
-		render: function(parent) {
-			this.nodes.parent = $(parent);
-			
-			this.nodes.parent.append('<a href="#" class="btn-authenticate">Login</a>');
-		},
-		*/
+    render: function(parent) {
+            this.nodes.parent = $(parent);
+
+            this.nodes.parent.append('<a href="#" class="btn-authenticate">Login</a>');
+    },
 		
     login: function(args) {
         var THIS = this;
@@ -87,8 +85,6 @@ winkstart.module('core', 'auth', {
             height: 320,
             position: 'center'
         });
-
-        winkstart.publish('sipservice.input_css');
     },
 
     recover_password: function(args) {
@@ -100,35 +96,41 @@ winkstart.module('core', 'auth', {
             height: 200,
             position: 'center'
         });
-
-        winkstart.publish('sipservice.input_css');
     },
 
 
 
-		authenticate: function() {
-			// A few things need to be done here
-			// 1) If we're not authenticated, do so
+    authenticate: function() {
+        // A few things need to be done here
+        // 1) If we're not authenticated, do so
 			
-			var _t = this;
-			amplify.request('auth.establish', {
-				username: '',
-				passwordhash: ''
-			}, function(data) {
-				_t.session.authenticated = true;
-				_t.session.token         = data.auth_token;
-				_t.session.expires       = data.expires;
-				alert('User authenticated');
-			});
-		}
-	},
-	function(args) {
-		winkstart.publish('subnav.add', { module: this.__module, label: 'Login', nav_category: 'category-1'});
-	},
-	{	activate: function(args) {
-			$(args.target).empty();
+        var _t = this;
+        amplify.request('auth.establish', {
+            username: '',
+            passwordhash: ''
+        }, function(data) {
+            _t.session.authenticated = true;
+            _t.session.token         = data.auth_token;
+            _t.session.expires       = data.expires;
+            alert('User authenticated');
+        });
+    }
+},
+function(args) {
+    winkstart.publish('subnav.add', {
+        module: this.__module,
+        label: 'Login',
+        nav_category: 'category-1'
+    });
+},
+{
+    activate: function(args) {
+        $(args.target).empty();
 			
-			this.templates.login.appendTo(args.target);
-			winkstart.publish('layout.updateLoadedModule', {label: 'User Authentication', module: this.__module});
-		}
-	});
+        this.templates.login.appendTo(args.target);
+        winkstart.publish('layout.updateLoadedModule', {
+            label: 'User Authentication',
+            module: this.__module
+            });
+    }
+});
