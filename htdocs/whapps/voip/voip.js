738c591695a7e8ba4ff290915887b556bd639d62
dschreiber <d@d-man.org>
2011-08-03 07:37:13 -0700

    Fix voip frontpage/homepage on click for PBX services.

--- htdocs/whapps/voip/voip.js
+++ htdocs/whapps/voip/voip.js
@@ -32,79 +32,67 @@ winkstart.module('voip', 'voip', {
         activate: function() {
             var THIS = this;
 
-            if(winkstart.apps['voip']['auth_token']) {
+            if (!winkstart.apps[this.__module].auth_token) {                    // Is this app authenticated?
+                winkstart.publish('auth.shared_auth', { app_name : this.__module });
+            } else if (!THIS.initialized) {                                 // Is this app initialized?
                 winkstart.registerResources(this.__whapp, this.config.resources);
 
-                //TODO: dynamic realm
-                var form_data = { 'shared_token': winkstart.apps['voip']['auth_token'], 'realm': 'testxav.pbx.2600hz.com' };
-                var rest_data = {};
-                rest_data.crossbar = true;
-                rest_data.data = form_data;
-
-                winkstart.putJSON('shared_auth', rest_data, function (json, xhr) {
-                    winkstart.apps['voip']['auth_token'] = json.auth_token;
-                    if (!THIS.initialized) {
-
-                        // Load the modules
-                        $.each(THIS.modules, function(k, v) {
-                            winkstart.module.loadModule('voip', v, function() {
-                                this.init(function() {
-                                    winkstart.log('VoIP: Initialized ' + v);
-                                });
-                            });
+                // Load the modules
+                $.each(THIS.modules, function(k, v) {
+                    winkstart.module.loadModule('voip', v, function() {
+                        this.init(function() {
+                            winkstart.log('VoIP: Initialized ' + v);
                         });
-                        
-                        // Display the navbar
-                        $('#ws-content').empty();
-                        THIS.templates.voip.tmpl({}).appendTo( $('#ws-content') );
-
-                        // Link the main buttons
-                        $('.options #users').click(function() {
-                            winkstart.publish('user.activate');
-                        });
-
-                        $('.options #devices').click(function() {
-                            winkstart.publish('device.activate');
-                        });
-
-                        $('.options #users').click(function() {
-                            winkstart.publish('user.activate');
-                        });
-
-                        $('.options #auto_attendant').click(function() {
-                            winkstart.publish('menu.activate');
-                        });
-
-                        $('.options #ring_groups').click(function() {
-                            winkstart.publish('callflow.activate');
-                        });
-
-                        $('.options #conferences').click(function() {
-                            winkstart.publish('conference.activate');
-                        });
-
-                        $('.options #registrations').click(function() {
-                            winkstart.publish('registration.activate');
-                        });
-
-                        $('.options #stats').click(function() {
-                            winkstart.publish('stats.activate');
-                        });
-
-                        $('.options #time_of_day').click(function() {
-                            winkstart.publish('timeofday.activate');
-                        });
-                        
-                        THIS.initialized = true;
-                    }
+                    });
                 });
-            
+
+                $('#ws-content').empty();
+                THIS.templates.voip.tmpl({}).appendTo( $('#ws-content') );
+
+                // Link the main buttons
+                $('.options #users').click(function() {
+                    winkstart.publish('user.activate');
+                });
+
+                $('.options #devices').click(function() {
+                    winkstart.publish('device.activate');
+                });
+
+                $('.options #users').click(function() {
+                    winkstart.publish('user.activate');
+                });
+
+                $('.options #auto_attendant').click(function() {
+                    winkstart.publish('menu.activate');
+                });
+
+                $('.options #ring_groups').click(function() {
+                    winkstart.publish('callflow.activate');
+                });
+
+                $('.options #conferences').click(function() {
+                    winkstart.publish('conference.activate');
+                });
+
+                $('.options #registrations').click(function() {
+                    winkstart.publish('registration.activate');
+                });
+
+                $('.options #stats').click(function() {
+                    winkstart.publish('stats.activate');
+                });
+
+                $('.options #time_of_day').click(function() {
+                    winkstart.publish('timeofday.activate');
+                });
+
+                THIS.initialized = true;
+            } else {
+                // Already initialized - just show main page
+
+                $('#ws-content').empty();
+                THIS.templates.voip.tmpl({}).appendTo( $('#ws-content') );
             }
-
-            //winkstart.registerResources(this.config.resources);
-
-            //winkstart.publish('layout.updateLoadedModule', {label: 'Device Management', module: this.__module});
-
         }
     }
 );
