61ce091493a133bb6eac5ffa52f5e46bb801bd20
Francis Genet <francis@2600hz.com>
2011-08-15 16:18:49 -0700

    Displaying the current API url on the VoIP whapps when we are clicking on the main icon

--- htdocs/whapps/voip/voip.js
+++ htdocs/whapps/voip/voip.js
@@ -1,100 +1,105 @@
 // This is the VoIP Services base application
 winkstart.module('voip', 'voip', {
-        css: {
-            voip: 'voip.css'
-        },
-        templates: {
-            voip: 'voip.html'
-        },
-
-        subscribe: {
-            'voip.activate' : 'activate',
-            'voip.initialized' : 'initialized'
-        }
+    css: {
+        voip: 'voip.css'
     },
-    function() {
-        // Loaded - add to nav bar
-        winkstart.publish('appnav.add', { 'name' : this.__module });
+    templates: {
+        voip: 'voip.html'
     },
-    {
-        is_initialized :   false,
-        modules :       ['account', 'media', 'device', 'callflow', 'conference', 'user', 'vmbox', 'menu', 'registration', 'resource', 'timeofday'],
 
-        initialized: function() {
-            var THIS = this;
+    subscribe: {
+        'voip.activate' : 'activate',
+        'voip.initialized' : 'initialized'
+    }
+},
+function() {
+    // Loaded - add to nav bar
+    winkstart.publish('appnav.add', {
+        'name' : this.__module
+        });
+},
+{
+    is_initialized :   false,
+    modules :       ['account', 'media', 'device', 'callflow', 'conference', 'user', 'vmbox', 'menu', 'registration', 'resource', 'timeofday'],
 
-            THIS.is_initialized = true;
+    initialized: function() {
+        var THIS = this;
 
-            winkstart.publish('subnav.show', THIS.__module);
+        THIS.is_initialized = true;
 
-            THIS.setup_page();
-        },
+        winkstart.publish('subnav.show', THIS.__module);
+
+        THIS.setup_page();
+    },
         
-        activate: function() {
-            var THIS = this,
-                mod_count;
+    activate: function() {
+        var THIS = this,
+        mod_count;
 
-            if (!THIS.is_initialized) {                                 // Is this app initialized?
-                // Load the modules
-                mod_count = THIS.modules.length;
-                $.each(THIS.modules, function(k, v) {
-                    winkstart.module.loadModule('voip', v, function() {
-                        this.init(function() {
-                            winkstart.log('VoIP: Initialized ' + v);
+        if (!THIS.is_initialized) {                                 // Is this app initialized?
+            // Load the modules
+            mod_count = THIS.modules.length;
+            $.each(THIS.modules, function(k, v) {
+                winkstart.module.loadModule('voip', v, function() {
+                    this.init(function() {
+                        winkstart.log('VoIP: Initialized ' + v);
                             
-                            if(!--mod_count) {
-                                winkstart.publish('voip.initialized', {});
-                            }
-                        });
+                        if(!--mod_count) {
+                            winkstart.publish('voip.initialized', {});
+                        }
                     });
                 });
-            } else {
-                THIS.setup_page();
-            }
-        },
-
-        setup_page: function() {
-            var THIS = this; 
-
-            $('#ws-content').empty();
-            THIS.templates.voip.tmpl({}).appendTo( $('#ws-content') );
-
-            // Link the main buttons
-            $('.options #users').click(function() {
-                winkstart.publish('user.activate');
             });
-
-            $('.options #devices').click(function() {
-                winkstart.publish('device.activate');
-            });
-
-            $('.options #users').click(function() {
-                winkstart.publish('user.activate');
-            });
-
-            $('.options #auto_attendant').click(function() {
-                winkstart.publish('menu.activate');
-            });
-
-            $('.options #ring_groups').click(function() {
-                winkstart.publish('callflow.activate');
-            });
-
-            $('.options #conferences').click(function() {
-                winkstart.publish('conference.activate');
-            });
-
-            $('.options #registrations').click(function() {
-                winkstart.publish('registration.activate');
-            });
-
-            $('.options #stats').click(function() {
-                winkstart.publish('stats.activate');
-            });
-
-            $('.options #time_of_day').click(function() {
-                winkstart.publish('timeofday.activate');
-            });
+                
+        } else {
+            THIS.setup_page();
         }
+    },
+
+    setup_page: function() {
+        var THIS = this; 
+
+        $('#ws-content').empty();
+        THIS.templates.voip.tmpl({}).appendTo( $('#ws-content') );
+        
+        $('#cur_api_url').append('You are currently using the API on: <b>'+ winkstart.apps['auth'].api_url +'</b>');
+
+        // Link the main buttons
+        $('.options #users').click(function() {
+            winkstart.publish('user.activate');
+        });
+
+        $('.options #devices').click(function() {
+            winkstart.publish('device.activate');
+        });
+
+        $('.options #users').click(function() {
+            winkstart.publish('user.activate');
+        });
+
+        $('.options #auto_attendant').click(function() {
+            winkstart.publish('menu.activate');
+        });
+
+        $('.options #ring_groups').click(function() {
+            winkstart.publish('callflow.activate');
+        });
+
+        $('.options #conferences').click(function() {
+            winkstart.publish('conference.activate');
+        });
+
+        $('.options #registrations').click(function() {
+            winkstart.publish('registration.activate');
+        });
+
+        $('.options #stats').click(function() {
+            winkstart.publish('stats.activate');
+        });
+
+        $('.options #time_of_day').click(function() {
+            winkstart.publish('timeofday.activate');
+        });
     }
+}
 );
