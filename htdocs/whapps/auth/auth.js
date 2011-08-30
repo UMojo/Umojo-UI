181d4f49781749f2d8bf26f5e45d2b274994c8b6
Jon Blanton <jon@2600hz.com>
2011-08-30 19:42:39 -0400

    WINKSTART-322: Fixed. Login now alerts about invalid credentials, and returns a general error for all other requests

--- htdocs/whapps/auth/auth.js
+++ htdocs/whapps/auth/auth.js
@@ -162,19 +162,27 @@ winkstart.module('auth', 'auth',
                     }
                 };
 
-                winkstart.putJSON('auth.user_auth', rest_data, function (json, xhr) {
-                    winkstart.apps['auth'].account_id = json.data.account_id;
-                    winkstart.apps['auth'].auth_token = json.auth_token;
-                    winkstart.apps['auth'].user_id = json.data.owner_id;
-                    winkstart.apps['auth'].realm = realm;
+                winkstart.putJSON('auth.user_auth', rest_data, function (data, status) {
+                        winkstart.apps['auth'].account_id = data.data.account_id;
+                        winkstart.apps['auth'].auth_token = data.auth_token;
+                        winkstart.apps['auth'].user_id = data.data.owner_id;
+                        winkstart.apps['auth'].realm = realm;
 
-                    $(dialogDiv).dialog('close');
-                    
-                    // Deleting the welcome message
-                    $('#ws-content').empty();
-                    
-                    winkstart.publish('auth.load_account');
-                });
+                        $(dialogDiv).dialog('close');
+                        
+                        // Deleting the welcome message
+                        $('#ws-content').empty();
+                        
+                        winkstart.publish('auth.load_account');
+                    },
+                    function(data, status) {
+                        if(status == '401' || status == '403') {
+                            alert('Invalid credentials, please check that your username and password are correct.');
+                        }
+                        else {
+                            alert('An error was encountered while attemping to process your request (Error: ' + status + ')');
+                        }
+                    });
             });
 
             $('a.register', dialogDiv).click(function(event) {
